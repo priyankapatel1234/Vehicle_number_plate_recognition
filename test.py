56ddb46f21dcc2d2ee6bcc93fc1f68b504cf0f89
Priyanka Patel <55329362+priyankapatel1234@users.noreply.github.com>
2020-10-11 23:28:29 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cv2      #install pip install opencv-python in anaconda prompt
+import cv2      # install pip install opencv-python following coomond on  anaconda prompt
 #import matplotlib
 #print(cv2.__version__)
 import pandas as pd
